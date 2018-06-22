29253696612fbc2b8ab0004a520d8f885f0c8ff5
Dries Goossens <driesgoossens93@gmail.com>
2018-06-22 19:40:21 +0200

    put in function createOpvolg

--- afspraken_naar_db.php
+++ afspraken_naar_db.php
@@ -155,34 +155,65 @@
 		}
 	}
 
-	/*
-	function create_calendar_appointment($date,$time,$name,$email,$phone,$remark,$type){
-		$client = getClient();
-		$service = new Google_Service_Calendar($client);
-		$calendarId = 'dietiste.borah@gmail.com';		
-		
-		if($type=="opvolg"){
-			$endTime = strtotime($time) + (30*60);
+	function createOpvolg($results,$previousEndTime){
+		foreach ($results->getItems() as $event) {
+			if(!($event->getSummary() == "Open")){
+				//Check begintijd met eind tijd vorige afspraak. Daarna "eindtijd" op eigen eindtijd zetten. 
+				//Op basis daarvan vrije momenten toevoegen aan de lijst met vrije uren (aantal minuten delen door 30 of 90)
+				$startDateTime = $event->start->dateTime;
+				$start = substr($startDateTime, 11, 5);						
+				if(strtotime($start) > strtotime($previousEndTime)){
+					$timeDifferenceInMinutes = (strtotime($start) - strtotime($previousEndTime))/60;
+					if(($timeDifferenceInMinutes/30) >= 1){ //afspraak 30 min
+						$noTime = false;
+						$amountOfAppointments = $timeDifferenceInMinutes/30;
+						for($i=0;$i<$amountOfAppointments;$i++){
+							$add = 30 + (30*$i);
+							$newStartTime = strtotime($previousEndTime) + (30*60*$i); 
+							printf("%s;", date("H:i",$newStartTime)); //TODO -> insert naar DB
+						}
+					}
+					/*elseif(($timeDifferenceInMinutes/90) >= 1)){ //afspraak van 90 min
+						$noTime = false;
+						$amountOfAppointments = $timeDifferenceInMinutes/30; //elke 30 min een afspraak
+						for($i=0;$i<($amountOfAppointments-2);$i++){
+							$add = 30 + (30*$i);
+							$newStartTime = strtotime($previousEndTime) + (30*60*$i); 
+							printf("%s;", date("H:i",$newStartTime)); //TODO -> insert naar DB
+						}
+					}*/
+				}
+				else{
+					//Do nothing, no time left
+				}
+				$previousEndTime = substr($event->getEnd()->dateTime,11,5);
+			}
 		}
-		else{
-			$endTime = strtotime($time) + (90*60);
-		}
-		$event = new Google_Service_Calendar_Event(array(
-		  'summary' => $name . ' '. $type,
-		  'description' => $name . ' - '.$remark.' - '.$email.' '.$phone.' '.$type,
-		  'start' => array(
-			'dateTime' => $date.'T'.$time.':00',
-			'timeZone' => 'Europe/Brussels',
-		  ),
-		  'end' => array(
-			'dateTime' => $date.'T'.date("H:i:s",$endTime),
-			'timeZone' => 'Europe/Brussels',
-		  ),
-		));
-
-		$calendarId = 'primary';
-		$event = $service->events->insert($calendarId, $event);
-	} */
+		//do the check for the last appointment & closing time
+		$endOpen=substr($endOpen, 11, 5);
+		if(strtotime($endOpen) > strtotime($previousEndTime)){
+			$timeDifferenceInMinutes = (strtotime($endOpen) - strtotime($previousEndTime))/60;
+			if(($timeDifferenceInMinutes/30) >= 1){ //afspraak 30 min
+				$noTime = false;
+				$amountOfAppointments = $timeDifferenceInMinutes/30;
+				for($i=0;$i<$amountOfAppointments;$i++){
+					$add = 30 + (30*$i);
+					$newStartTime = strtotime($previousEndTime) + (30*60*$i); 
+					printf("%s;", date("H:i",$newStartTime)); //TODO -> insert naar DB
+				}
+			}
+			/*
+			elseif(($timeDifferenceInMinutes/90) >= 1)){ //afspraak van 90 min
+				$noTime = false;
+				$amountOfAppointments = $timeDifferenceInMinutes/30; //elke 30 min een afspraak
+				for($i=0;$i<($amountOfAppointments-2);$i++){
+					$add = 30 + (30*$i);
+					$newStartTime = strtotime($previousEndTime) + (30*60*$i); 
+					printf("%s;", date("H:i",$newStartTime)); //TODO -> insert naar DB
+				}
+			} */
+		}	
+	}
 
 	// Get the API client and construct the service object.
 	$client = getClient();
@@ -228,67 +259,10 @@
 						
 						$startOpen=substr($startOpen, 11, 5);
 						$previousEndTime = $startOpen; //First time, difference between Open "openingtime" and first appointment has to be found
-						//$opvolg=false;
-						foreach ($results->getItems() as $event) {
-							if(!($event->getSummary() == "Open")){
-								//Check begintijd met eind tijd vorige afspraak. Daarna "eindtijd" op eigen eindtijd zetten. 
-								//Op basis daarvan vrije momenten toevoegen aan de lijst met vrije uren (aantal minuten delen door 30 of 90)
-								$startDateTime = $event->start->dateTime;
-								$start = substr($startDateTime, 11, 5);						
-								if(strtotime($start) > strtotime($previousEndTime)){
-									$timeDifferenceInMinutes = (strtotime($start) - strtotime($previousEndTime))/60;
-									if(($timeDifferenceInMinutes/30) >= 1){ //afspraak 30 min
-										$noTime = false;
-										$amountOfAppointments = $timeDifferenceInMinutes/30;
-										for($i=0;$i<$amountOfAppointments;$i++){
-											$add = 30 + (30*$i);
-											$newStartTime = strtotime($previousEndTime) + (30*60*$i); 
-											printf("%s;", date("H:i",$newStartTime)); //TODO -> insert naar DB
-										}
-									}
-									/*elseif(($timeDifferenceInMinutes/90) >= 1)){ //afspraak van 90 min
-										$noTime = false;
-										$amountOfAppointments = $timeDifferenceInMinutes/30; //elke 30 min een afspraak
-										for($i=0;$i<($amountOfAppointments-2);$i++){
-											$add = 30 + (30*$i);
-											$newStartTime = strtotime($previousEndTime) + (30*60*$i); 
-											printf("%s;", date("H:i",$newStartTime)); //TODO -> insert naar DB
-										}
-									}*/
-								}
-								else{
-									//Do nothing, no time left
-								}
-								$previousEndTime = substr($event->getEnd()->dateTime,11,5);
-							}
-						}
-						//do the check for the last appointment & closing time
-						$endOpen=substr($endOpen, 11, 5);
-						if(strtotime($endOpen) > strtotime($previousEndTime)){
-							$timeDifferenceInMinutes = (strtotime($endOpen) - strtotime($previousEndTime))/60;
-							if(($timeDifferenceInMinutes/30) >= 1){ //afspraak 30 min
-								$noTime = false;
-								$amountOfAppointments = $timeDifferenceInMinutes/30;
-								for($i=0;$i<$amountOfAppointments;$i++){
-									$add = 30 + (30*$i);
-									$newStartTime = strtotime($previousEndTime) + (30*60*$i); 
-									printf("%s;", date("H:i",$newStartTime)); //TODO -> insert naar DB
-								}
-							}
-							/*
-							elseif(($timeDifferenceInMinutes/90) >= 1)){ //afspraak van 90 min
-								$noTime = false;
-								$amountOfAppointments = $timeDifferenceInMinutes/30; //elke 30 min een afspraak
-								for($i=0;$i<($amountOfAppointments-2);$i++){
-									$add = 30 + (30*$i);
-									$newStartTime = strtotime($previousEndTime) + (30*60*$i); 
-									printf("%s;", date("H:i",$newStartTime)); //TODO -> insert naar DB
-								}
-							} */
-						}				
+						createOpvolg($results,$previousEndTime);
 					}
 					else{
-						print "Geen tijdstippen vrij op deze datum.\n";
+						print "Geen afspraak met titel open.\n";
 					}
 				}
 
