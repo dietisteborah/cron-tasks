acff7aba25d32cd9660c22dda133ead85776d682
Dries Goossens <driesgoossens93@gmail.com>
2018-07-13 15:55:19 +0200

    adding debug code + code cleanup

--- reminder_mail.php
+++ reminder_mail.php
@@ -1,122 +1,6 @@
 <?php
 	require_once '/home/borahv1q/vendor/autoload.php';
 	putenv('GOOGLE_APPLICATION_CREDENTIALS=/home/borahv1q/borah-secrets/client_secret.json');
-
-	function createEerste($results,$previousEndTime,$endOpen){
-		//Database functionality
-		$string = file_get_contents("/home/borahv1q/borah-secrets/pw.txt");
-		$string = str_replace(array("\r", "\n"), '', $string);
-		$link = mysqli_connect("localhost", "borahv1q", $string , "borahv1q_Agenda");
-		if (!$link) {
-			echo "Error: Unable to connect to MySQL." . PHP_EOL;
-			echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-			echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-			$errordate = date('d.m.Y h:i:s'); 
-			error_log($errordate."--"."Error: Unable to connect to MySQL." . PHP_EOL ."\n", 3, "/home/borahv1q/logs/php-reminder_mail.log");
-			error_log($errordate."--"."Debugging errno: " . mysqli_connect_errno() . PHP_EOL ."\n", 3, "/home/borahv1q/logs/php-reminder_mail.log");
-			error_log($errordate."--"."Debugging error: " . mysqli_connect_error() . PHP_EOL ."\n", 3, "/home/borahv1q/logs/php-reminder_mail.log");
-			exit;
-		}
-		//echo "Connect to mysql.\n" . PHP_EOL;
-		$errordate = date('d.m.Y h:i:s'); 
-		error_log($errordate."--"."Executing function createEerste and connected to DB\n", 3, "/home/borahv1q/logs/php-reminder_mail.log");
-
-
-		$app_date_end = "";
-		$open=false;
-		$appointmentsInList = 0;
-		foreach ($results->getItems() as $event) {
-			$appointmentsInList = $appointmentsInList +1;
-			$startDateTime = $event->start->dateTime; //needed if there is only 1 appointment
-			$app_date_end = substr($startDateTime, 0, 10); //needed if there is only 1 appointment
-			if(!($event->getSummary() == "Open")){
-				$errordate = date('d.m.Y h:i:s'); 
-				error_log($errordate."--"."(createEerste - $event->getSummary() == \"Open\")\n", 3, "/home/borahv1q/logs/php-reminder_mail.log");	
-				//Check begintijd met eind tijd vorige afspraak. Daarna "eindtijd" op eigen eindtijd zetten. 
-				//Op basis daarvan vrije momenten toevoegen aan de lijst met vrije uren (aantal minuten delen door 30 of 90)
-				$start = substr($startDateTime, 11, 5);		
-				$app_date = substr($startDateTime, 0, 10);
-				printf("SDL: %s; STL: %s;", $app_date,$start);
-				if(strtotime($start) > strtotime($previousEndTime)){
-					$timeDifferenceInMinutes = (strtotime($start) - strtotime($previousEndTime))/60;
-					if(($timeDifferenceInMinutes/30) >= 1){ //afspraak 90 min
-						$noTime = false;
-						$amountOfAppointments = $timeDifferenceInMinutes/30; //afspraak kan elke 30min geplaatst worden
-						for($i=0;$i<$amountOfAppointments-2;$i++){ //-2 om te zorgen dat er op tijd gestopt wordt met afspraken maken
-							$add = 30 + (30*$i);
-							$newStartTime = strtotime($previousEndTime) + (30*60*$i); 
-							$db_endTime = $newStartTime + (90*60);
-							//printf("%s;", date("H:i",$newStartTime)); //TODO -> insert naar DB
-									$sql = "INSERT INTO afspraken (opvolg, date, startTime, endTime)
-									VALUES (0,'".$app_date."','".date("H:i",$newStartTime).":00','".date("H:i",$db_endTime).":00')"; //opvolg = 0 want geen opvolg afspr
-									if (mysqli_query($link, $sql)) {
-										echo "_OK_";
-									} else {
-										echo "Error: " . $sql . "<br>" . mysqli_error($link);
-									}
-						}
-					}
-				}
-				else{
-					//Do nothing, no time left
-				}
-				$previousEndTime = substr($event->getEnd()->dateTime,11,5);
-				$open=true;
-			}
-		}
-		if($open || $appointmentsInList ==1){
-			//do the check for the last appointment & closing time
-			$endOpen=substr($endOpen, 11, 5);
-			printf("EDL: %s; ETL: %s;", $app_date_end,$endOpen);
-			if(strtotime($endOpen) > strtotime($previousEndTime)){
-				$errordate = date('d.m.Y h:i:s'); 
-				error_log($errordate."--"."createEerste - strtotime($endOpen) > strtotime($previousEndTime)\n", 3, "/home/borahv1q/logs/php-reminder_mail.log");	
-				$timeDifferenceInMinutes = (strtotime($endOpen) - strtotime($previousEndTime))/60;
-				if(($timeDifferenceInMinutes/90) >= 1){ //afspraak 90 min
-					$noTime = false;
-					$amountOfAppointments = $timeDifferenceInMinutes/30; //afspraak kan elke 30min geplaatst worden
-					for($i=0;$i<$amountOfAppointments-2;$i++){ //-2 om te zorgen dat er op tijd gestopt wordt met afspraken maken
-						$add = 30 + (30*$i);
-						$newStartTime = strtotime($previousEndTime) + (30*60*$i); 
-						$db_endTime = $newStartTime + (90*60);
-						$sql = "INSERT INTO afspraken (opvolg, date, startTime, endTime)
-						VALUES (0,'".$app_date_end."','".date("H:i",$newStartTime).":00','".date("H:i",$db_endTime).":00')"; //opvolg = 0 want geen opvolg afspr
-						if (mysqli_query($link, $sql)) {
-							echo "_OK_";
-						} else {
-							echo "Error: " . $sql . "<br>" . mysqli_error($link);
-						}
-					}
-				}
-			}
-		}
-		mysqli_close($link);
-	}
-	function cleanDB(){
-		//Database functionality
-		$string = file_get_contents("/home/borahv1q/borah-secrets/pw.txt");
-		$string = str_replace(array("\r", "\n"), '', $string);
-		$link = mysqli_connect("localhost", "borahv1q", $string , "borahv1q_Agenda");
-		if (!$link) {
-			echo "Error: Unable to connect to MySQL." . PHP_EOL;
-			echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-			echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-			$errordate = date('d.m.Y h:i:s'); 
-			error_log($errordate."--"."Error: Unable to connect to MySQL." . PHP_EOL ."\n", 3, "/home/borahv1q/logs/php-reminder_mail.log");
-			error_log($errordate."--"."Debugging errno: " . mysqli_connect_errno() . PHP_EOL ."\n", 3, "/home/borahv1q/logs/php-reminder_mail.log");
-			error_log($errordate."--"."Debugging error: " . mysqli_connect_error() . PHP_EOL ."\n", 3, "/home/borahv1q/logs/php-reminder_mail.log");
-			exit;
-		}
-		$sql = "DELETE FROM afspraken";
-		$errordate = date('d.m.Y h:i:s'); 
-		if (mysqli_query($link, $sql)) {
-			error_log($errordate."--"."OK: query".$sql."\n", 3, "/home/borahv1q/logs/php-reminder_mail.log");
-		} else {
-			error_log($errordate."--"."Error: query".$sql."--Message--". mysqli_connect_error() . PHP_EOL ."\n", 3, "/home/borahv1q/logs/php-reminder_mail.log");
-		}
-
-		mysqli_close($link);
-	}	
 	
 	$errordate = date('d.m.Y h:i:s'); 
 	error_log($errordate."--"."Starting reminder_mail.php script.\n", 3, "/home/borahv1q/logs/php-reminder_mail.log");
@@ -141,6 +25,7 @@
 		exit;
 	}
 	//get all to be send reminders
+	error_log($errordate."--"."today_string: " . $today_string ."\n", 3, "/home/borahv1q/logs/php-reminder_mail.log");
 	$sql = "SELECT * FROM reminders where reminder_date <=\"".$today_string;
 	$result = mysqli_query($link, $sql);
 	if (mysqli_num_rows($result) > 0) {
